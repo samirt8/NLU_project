6418f271c8c68a9da8da88f24f060b828d0fb57e
samir tanfous <stanfous@koios.gso.lan>
2020-12-26 16:04:35 +0100

    modify package setup

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pkg-Samir-Tanfous-NLU",
-    version="0.0.3",
+    version="0.0.4",
     author="Samir Tanfous",
     author_email="samir.tanfous@gmail.com",
     description="RUN NLU pipeline",
